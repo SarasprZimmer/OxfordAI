5b08248ffd9978ee2638527d560d8ec71ad5f4eb
SarasprZimmer <sara2024aiacc@gmail.com>
2025-04-20 13:29:14 +0400

    render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Download and extract Chromium
-wget https://storage.googleapis.com/chromium-browser-snapshots/Linux_x64/1210785/chrome-linux.zip -O /tmp/chromium.zip
-unzip /tmp/chromium.zip -d /tmp/
-mv /tmp/chrome-linux /opt/render/project/src/chrome-linux
+# Install Chromium
+CHROME_VERSION="121.0.6167.85"  # Pick a stable version
+mkdir -p .chromium
+cd .chromium
+curl -LO "https://storage.googleapis.com/chromium-browser-snapshots/Linux_x64/1210655/chrome-linux.zip"
+unzip chrome-linux.zip
+cd ..
